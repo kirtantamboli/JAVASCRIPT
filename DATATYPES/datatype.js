3c769b438502666e1c75fb42232d101b94567180
Kirtan Tamboli <kirtantamboli38@gmail.com>
2024-08-30 14:04:35 +0530

    datatypes

---
+++ DATATYPES/datatype.js
@@ -0,0 +1,17 @@
+var bool = true ;
+console.log(typeof bool);
+
+var num  = 5;
+console.log(typeof num);
+
+var alphabet = 'abc';
+console.log(typeof alphabet);
+
+var x = null  ;
+console.log(typeof x); 
+
+var xy ;
+console.log(typeof xy);
+
+var bigint =123456777890n;
+console.log(typeof bigint);
