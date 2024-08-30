4d4a42def5d40bbbdbbf3246a4b5941c1c44e3cd
Kirtan Tamboli <kirtantamboli38@gmail.com>
2024-08-30 14:09:18 +0530

    .

--- DAY_1/namasteworld.js
+++
@@ -1,8 +0,0 @@
-let namastebtn = document.querySelector('button');
-namastebtn.addEventListener('click',showMsg);
-
-function showMsg(){
-    let name =prompt('enter name of user');
-    namastebtn.textContent='NAMASTE 🫶'+ name;
-    alert("namaste world !");
-}
