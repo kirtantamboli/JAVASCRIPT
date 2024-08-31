28ef19c29c71c5b2ce245e9e63d3730b1fe4e8c5
Kirtan Tamboli <kirtantamboli38@gmail.com>
2024-08-31 09:23:46 +0530

    variable

---
+++ VARIABLES/variable.js
@@ -0,0 +1,41 @@
+//declaring a variable
+let myname;
+let age;
+
+//initializing a variable
+myname ='chris';
+age = 37;
+console.log(myname,age);
+
+//camelcase
+let firstName ="Kirtan";
+
+//pascalcase
+let LastName = "Tamboli";
+
+//snakecase
+let middle_name = "raj";
+
+ //in const variable when we declare it we or user can't change its value
+ //using constant (const keyword )
+ const price = 99 ;
+
+ //course name string
+ var coursename = "dsa js";
+
+ //card number 
+ var cardnum = "1234567890";
+
+ //payment status (boolean)
+ var ispaid = true;
+
+ //taking input from user
+ var modeofpayment ;
+ modeofpayment = prompt("mode of payment");
+
+//output to userr
+alert(firstName + "successfully bought" + coursename + "using"+ modeofpayment);
+
+
+
+
