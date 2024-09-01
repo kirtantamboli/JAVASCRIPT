99d5121d40aae2192caa126c19ebe677b879f681
Kirtan Tamboli <kirtantamboli38@gmail.com>
2024-09-01 14:08:20 +0530

    array

---
+++ LOOPS/array.js
@@ -0,0 +1,11 @@
+//arrays
+let names =["a","b","c","d"];
+for(let index in names){
+    console.log(index,names[index]);
+}
+//for of loop
+let name =["a","b","c","d"];
+for(let name of names){
+    console.log(name);
+}
+ 
