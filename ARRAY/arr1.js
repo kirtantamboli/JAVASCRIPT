5aa08f8f8c2eaf21a10f4b799a2566cde8e648e2
Kirtan Tamboli <kirtantamboli38@gmail.com>
2024-09-02 10:06:04 +0530

    array1

---
+++ ARRAY/arr1.js
@@ -0,0 +1,12 @@
+// Creating an Empty Array
+let names = [];
+console.log(names);
+
+// Creating an Array and Initializing with Values
+let courses = ["HTML", "CSS", "Javascript", "React"];
+
+console.log(courses);
+
+//practice
+let num = ["1","2","3","4","5"];
+console.log(num);
