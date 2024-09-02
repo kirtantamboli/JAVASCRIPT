b7c818540791a5e7770b6e320188132fd2de1a66
Kirtan Tamboli <kirtantamboli38@gmail.com>
2024-09-02 09:56:07 +0530

    fnc3

---
+++ FUNCTIONS/func3.js
@@ -0,0 +1,18 @@
+function welcomeMsg(name) {
+    return ("Hello " + name + " welcome to myworld");
+}
+// creating a variable
+let nameVal = "Admin";
+
+// calling the function
+console.log(welcomeMsg(nameVal));
+
+
+//practice
+
+function square(num){
+    return (num * num) ;
+}
+let num = 5 ;
+
+console.log(square(num));
