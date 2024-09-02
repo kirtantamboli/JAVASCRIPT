ebea12c68dfa93bd1a50f0e7752c79697de888cb
Kirtan Tamboli <kirtantamboli38@gmail.com>
2024-09-02 10:24:55 +0530

    arrays

--- ARRAY/arr1.js
+++ ARRAY/arr1.js
@@ -10,3 +10,30 @@ console.log(courses);
 //practice
 let num = ["1","2","3","4","5"];
 console.log(num);
+
+// Creating an Array and Initializing with Values  
+
+// Accessing Array Elements
+console.log(num[0]);
+console.log(num[1]); 
+
+// Creating an Array and Initializing with Values
+let cour = ["HTML", "CSS", "JavaScript", "React"];
+
+// Accessing First Array Elements
+let firstItem = cour[0];
+
+console.log("First Item: ", firstItem);
+
+// Accessing Last Array Elements
+let lastItem = cour[cour.length - 1];
+
+console.log("last Item: ", lastItem);
+
+// Creating an Array and Initializing with Values
+let subj = ["HTML", "CSS", "Javascript", "React"];
+console.log(subj);
+
+subj[1]= "Bootstrap";
+console.log(subj);
+
