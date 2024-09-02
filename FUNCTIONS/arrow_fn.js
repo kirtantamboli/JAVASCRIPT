835110d19ab35246768e2c743eddd00bb8d05799
Kirtan Tamboli <kirtantamboli38@gmail.com>
2024-09-02 09:52:57 +0530

    arrow fn

---
+++ FUNCTIONS/arrow_fn.js
@@ -0,0 +1,15 @@
+// Normal function for multiplication
+// of two numbers
+function multiply(a, b) {
+    return a * b;
+}
+console.log(multiply(3, 5));
+
+
+// Arrow function for multiplying two numbers
+value = (a, b) => a * b;
+console.log(value(3, 5));
+
+//practice
+num=(x,y)=>x*y;
+console.log(num(5,5));
