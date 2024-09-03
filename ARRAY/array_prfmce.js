24d6df0c611f09e301bcbb575b7469e68d3604a2
Kirtan Tamboli <kirtantamboli38@gmail.com>
2024-09-03 14:08:30 +0530

    array

---
+++ ARRAY/array_prfmce.js
@@ -0,0 +1,16 @@
+// push() method
+let Animals = ["Tiger", "Giraffe", "Horse", "Deer"];
+Animals.push("Zebra");
+console.log(Animals);
+
+// pop() method
+let Animals2 = ["Tiger", "Giraffe", "Horse", "Deer"];
+Animals2.pop();
+console.log(Animals2);
+
+// shift() method
+let Animals3 = ["Tiger", "Giraffe", "Horse", "Deer"];
+Animals3.shift();
+console.log(Animals3);
+
+
