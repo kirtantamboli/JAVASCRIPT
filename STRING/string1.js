3ba03a1ecd8133a36d98bdd533e24cc7db13ef14
Kirtan Tamboli <kirtantamboli38@gmail.com>
2024-09-04 09:48:28 +0530

    string manupulation

---
+++ STRING/string1.js
@@ -0,0 +1,21 @@
+//string length 
+var aplhabet = "game of thrones";
+console.log(aplhabet.length);
+
+//string excess elements 
+console.log(aplhabet[0]);
+console.log(aplhabet[aplhabet.length-1]);
+
+//finds index location of elements
+console.log(aplhabet.indexOf("of"));
+
+//slice substring
+console.log(aplhabet.slice(5,7));
+
+//uppercase
+var namm = "my name is kirtan";
+console.log(namm.toUpperCase());
+
+//lowercase
+var namm = "MY NAME IS KIRTAN";
+console.log(namm.toLowerCase());
