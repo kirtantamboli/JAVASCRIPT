f83592eb437255a8659ff49164af24786f123297
Kirtan Tamboli <kirtantamboli38@gmail.com>
2024-09-07 23:26:23 +0530

    promises

--- PROMISES/create.js
+++ PROMISES/create.js
@@ -10,3 +10,12 @@ let myPromise = new Promise(function(resolve, reject) {
     }
   });
   
+  //using a promise
+  myPromise.then(function(message) {
+    // This runs if the promise is fulfilled
+      console.log(message); 
+    }).catch(function(error) {
+    // This runs if the promise is rejected
+      console.log(error); 
+    });
+    
