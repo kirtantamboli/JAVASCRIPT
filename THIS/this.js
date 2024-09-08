214d3e6d5e68674f98f29d353b084b94d8f95073
Kirtan Tamboli <kirtantamboli38@gmail.com>
2024-09-08 22:35:29 +0530

    this fn

---
+++ THIS/this.js
@@ -0,0 +1,10 @@
+const person = {
+    name: 'kirtan',
+    age: 20,
+
+    greet(){
+        console.log('Hello, my name is ' + this.name + ' and I am '+ this.age +' years old.');
+    }
+};
+
+person.greet(); 
