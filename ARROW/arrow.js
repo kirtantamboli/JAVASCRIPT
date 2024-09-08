c7ecef9903fd6064e9b35415769c0288d2ee2364
Kirtan Tamboli <kirtantamboli38@gmail.com>
2024-09-08 22:29:42 +0530

    arrow fn

--- ARROW/arrow.js
+++ ARROW/arrow.js
@@ -1,14 +1,20 @@
 //An arrow function without parameters is defined using empty parentheses (). This is useful when you need a function that doesn’t require any arguments.
 
 //ARROW FUNCTION without parameter
-const gfg = () => {
+const fn = () => {
     console.log( "Hi from kirtan" );
 }
 
-gfg();
+fn();
 
 //ARROW FUNCTION WITH SINGLE PARAMETER
 const square = x => x*x;
 console.log(square(4));
 // output: 16
 
+//arrow fn with multiple parameter
+const abb = ( x, y, z ) => {
+    console.log( x + y + z )
+}
+
+abb( 10, 20, 30 );
