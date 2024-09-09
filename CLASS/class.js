ccf910ecbeab64df009e9549c28c7c57b7a61054
Kirtan Tamboli <kirtantamboli38@gmail.com>
2024-09-09 09:33:38 +0530

    class fn

---
+++ CLASS/class.js
@@ -0,0 +1,9 @@
+//in class -- it uses pascal case
+class Product {
+    constructor(itemname,price,discount,productcode){
+        this.itemname = itemname ;
+        this.price = price;
+        this.discount = discount;
+        this.productcode = productcode;
+    }
+}
--- PROMISES/using.js
+++
@@ -1,8 +0,0 @@
-myPromise.then(function(message) {
-    // This runs if the promise is fulfilled
-      console.log(message); 
-    }).catch(function(error) {
-    // This runs if the promise is rejected
-      console.log(error); 
-    });
-    
