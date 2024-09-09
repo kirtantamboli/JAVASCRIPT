3b561d799dff6f215a7fe01a744dd38807c9e9e5
Kirtan Tamboli <kirtantamboli38@gmail.com>
2024-09-09 09:35:38 +0530

    modified

--- CLASS/class.js
+++ CLASS/class.js
@@ -1,4 +1,5 @@
 //in class -- it uses pascal case
+//declaring a class
 class Product {
     constructor(itemname,price,discount,productcode){
         this.itemname = itemname ;
@@ -7,3 +8,7 @@ class Product {
         this.productcode = productcode;
     }
 }
+
+let pencil = new Product("pencil",20,2,p10);
+
+//now using it with expression
