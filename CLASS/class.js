c6ff989b5efd615a4e02eac0a6f34ccf47df9db6
Kirtan Tamboli <kirtantamboli38@gmail.com>
2024-09-09 09:45:37 +0530

    modified

--- CLASS/class.js
+++ CLASS/class.js
@@ -12,3 +12,26 @@ class Product {
 let pencil = new Product("pencil",20,2,p10);
 
 //now using it with expression
+
+const Product1 = class Product1{
+        constructor(itemname,price,discount,productcode){
+            this.itemname = itemname ;
+            this.price = price;
+            this.discount = discount;
+            this.productcode = productcode;
+        }
+        //getter and setter method
+        get getDiscountValue(){
+            return this.discount;
+        }
+        set setDiscountValue(value){
+            this.discount = value; 
+        }
+        //method 
+        DiscountValue(){
+            return this.discount*this.price/100; 
+        }
+
+    }; 
+
+let chair = new Product1('chair',499,15,'c10');
