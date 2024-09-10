2af42748ce143b0664417e400836d5a494ff2094
Kirtan Tamboli <kirtantamboli38@gmail.com>
2024-09-10 09:49:57 +0530

    array modified

--- ARRAY/array_prfmce.js
+++ ARRAY/array_prfmce.js
@@ -4,13 +4,21 @@ Animals.push("Zebra");
 console.log(Animals);
 
 // pop() method
+//returns last value or deletes
 let Animals2 = ["Tiger", "Giraffe", "Horse", "Deer"];
 Animals2.pop();
 console.log(Animals2);
 
 // shift() method
+//removes elements from the array at starting
 let Animals3 = ["Tiger", "Giraffe", "Horse", "Deer"];
 Animals3.shift();
 console.log(Animals3);
 
+//unshift() method
+// it adds new elements in an array at the starting and increases its arr length
+
+let Animals4 = ["Tiger", "Giraffe", "Horse", "Deer"];
+Animals4.unshift(0);
+console.log(Animals4);
 
