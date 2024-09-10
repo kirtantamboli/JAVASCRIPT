f6aca2800d0d0a092ee897471096e43c1d2a9680
Kirtan Tamboli <kirtantamboli38@gmail.com>
2024-09-10 10:57:02 +0530

    slice

--- ARRAY/concate.js
+++ ARRAY/concate.js
@@ -1,4 +1,12 @@
+//concate method
 let name1 = ['a','b','c'];
 let name2 = ['d','e','f'];
 console.log(name1.concat('kirtan','tamboli'));
 console.log(name1.concat(name2));
+
+//slice method 
+let num1 = ['1','2','3'];
+let num2 = ['4','5','6'];
+let num3 = num1.concat(num2)
+console.log(num3.slice(3));
+
