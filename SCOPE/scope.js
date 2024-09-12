601d5855a4f4ea1830ac03a5a9c1af36d777c809
Kirtan Tamboli <kirtantamboli38@gmail.com>
2024-09-12 23:42:18 +0530

    .

---
+++ SCOPE/scope.js
@@ -0,0 +1,12 @@
+// the scope is by default global
+// var name = 'anand';
+
+// function sayname(){
+//     var marks = 10;
+//     console.log(name);
+// }
+// sayname()
+// console.log(marks);//it will give error as it is executed out of scope
+
+//local scope
+  
