bfa6a1751469d7496012584dae7e29bb0d6f9a4d
Kirtan Tamboli <kirtantamboli38@gmail.com>
2024-09-12 15:21:41 +0530

    eventlistener

---
+++ EVENT_LISTENER/event_list.js
@@ -0,0 +1,4 @@
+//SYNTAX
+//element.addEventListener(event, function, useCapture);  
+
+//Note: Do not use any prefix such as "on" with the parameter value. For example, Use "click" instead of using "onclick".
