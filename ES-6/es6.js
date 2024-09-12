d9b991079b106ecf3e5ad9d4c164fe0779a67abf
Kirtan Tamboli <kirtantamboli38@gmail.com>
2024-09-12 10:35:00 +0530

    modified es6

--- ES-6/es6.js
+++ ES-6/es6.js
@@ -1,2 +1,3 @@
 //ES6
-//Javascript ES6 has been around for a few years now, and it allows us to write code in a clever way which basically makes the code more modern and more readable. It’s fair to say that with the use of ES6 features we write less and do more, hence the term ‘write less, do more’ definitely suits ES6. 
+//Javascript ES6 has been around for a few years now, and it allows us to write code in a clever way which basically makes the code more modern and more readable. 
+//It’s fair to say that with the use of ES6 features we write less and do more, hence the term ‘write less, do more’ definitely suits ES6. 
