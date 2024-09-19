600bddc76e5d6c3f1e428464584fbf663d505748
Kirtan Tamboli <kirtantamboli38@gmail.com>
2024-09-19 10:00:14 +0530

    map ex

---
+++ MAP/ex.js
@@ -0,0 +1,18 @@
+let map1 = new Map([
+    [1, 10],
+    [2, 20],
+    [3, 30],
+    [4,40]
+]);
+
+console.log("Map1");
+console.log(map1);
+
+let map2 = new Map([
+    ["firstname", "kirtan"],
+    ["lastname", "tamboli"],
+    ["college", "techno india njr"]
+]);
+
+console.log("Map2");
+console.log(map2);
